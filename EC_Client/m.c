66567d719cfb63909b33dc86a87cba9389631c12
xusongqi <xusongqi@foxmail.com>
2014-05-27 10:50:25 +0800

    client code-files

---
+++ EC_Client/m.c
@@ -0,0 +1,45 @@
+/* 
+* Author:		xusongqi@live.com
+* 
+* Created Time: 2014年05月26日 星期一 15时10分11秒
+* 
+* FileName:     ecrypt.h
+* 
+* Description:  
+*
+*/
+
+#ifndef _ENCRYPT_H_
+#define _ENCRYPT_H_
+
+#include "EC_client_include.h"
+
+//char * md5(unsigned char * str, char * buff);
+
+//char * md5(unsigned char * str, char * buff)
+int main(void)
+{
+	MD5_CTX ctx;
+	unsigned char md5[16] = {0};
+	unsigned char * str = "asdf";
+	char buff[33] = {'\0'},
+		 tmp[3] = {'\0'};
+	int i;
+
+	MD5_Init(&ctx);
+
+	MD5_Update(&ctx, str, strlen(str));
+
+	MD5_Final(md5, &ctx);
+
+	for(i = 0; i<16; i++)
+	{
+		sprintf(tmp, "%02x", md5[i]);
+		strcat(buff, tmp);
+	}
+	printf("%s\n",buff);
+
+	//return buff;
+}
+
+#endif
